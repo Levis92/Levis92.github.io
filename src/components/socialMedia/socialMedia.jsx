4e7f99ad4b3f945343135e6cfeba7e72575cb522
Anton Levholm <anton.badminton@gmail.com>
2018-09-03 13:22:51 +0200

    Add more social media

--- src/components/socialMedia/socialMedia.jsx
+++ src/components/socialMedia/socialMedia.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub, faTwitter, faDribbble } from '@fortawesome/free-brands-svg-icons';
 
 import { SocialMediaContainer } from "./socialMedia.styles";
 
@@ -8,9 +8,24 @@ import SocialMediaIcon from "../socialMediaIcon/socialMediaIcon";
 
 const media = [
   {
+    icon: faGithub,
+    url: "https://github.com/Levis92",
+    hoverColor: "#24292e"
+  },
+  {
     icon: faLinkedin,
     url: "https://www.linkedin.com/in/anton-levholm/",
     hoverColor: "#006fa6"
+  },
+  {
+    icon: faTwitter,
+    url: "https://twitter.com/ALevholm",
+    hoverColor: "#1da1f2"
+  },
+  {
+    icon: faDribbble,
+    url: "https://dribbble.com/levholm",
+    hoverColor: "#ea4c89"
   }
 ];
 
